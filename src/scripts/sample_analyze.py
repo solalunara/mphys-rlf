3bc16b4d1119f24f547805033b203f45e47b6258
Solalunara <greenberg04@gmail.com>
2025-10-21 12:25:00 +0100

    fix unit errors and beam width in sample analysis and plot final result

--- src/scripts/sample_analyze.py
+++ src/scripts/sample_analyze.py
@@ -4,6 +4,7 @@ import bdsf;
 from astropy.io import fits;
 from astropy.io.fits import ImageHDU;
 import numpy as np;
+import matplotlib.pyplot as plt;
 
 # Add the src directory to Python path so we can import modules
 src_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,14 +30,16 @@ fig.savefig( "sample.png" );
 #so loop through all the files and save them to samples/
 for i in range( samples.shape[ 0 ] ):
     hdul = fits.HDUList();
-    hdu = fits.PrimaryHDU( samples[ i, -1, 0, :, : ] );
+    hdu = fits.PrimaryHDU( samples[ i, -1, 0, ::-1, : ] );
 
     #add CTYPE headers so PyBDSF doesn't complain
     #and dashes to conform to FITS standard
-    hdu.header[ "CTYPE1" ] = "RA---   ";
-    hdu.header[ "CTYPE2" ] = "DEC--   ";
-    hdu.header[ "CDELT1" ] = 1.5; #arcseconds?
-    hdu.header[ "CDELT2" ] = 1.5;
+    hdu.header[ "CTYPE1" ] = "RA---TAN";
+    hdu.header[ "CTYPE2" ] = "DEC--TAN";
+    hdu.header[ "CDELT1" ] = 1.5 * 0.00027778;
+    hdu.header[ "CDELT2" ] = 1.5 * 0.00027778;
+    hdu.header[ "CUNIT1" ] = "deg";
+    hdu.header[ "CUNIT2" ] = "deg";
 
 
     hdul = fits.HDUList( [ hdu ] );
@@ -46,7 +49,7 @@ for i in range( samples.shape[ 0 ] ):
     # have pybdsf read the image and save to samples/fit_samples/
     img = bdsf.process_image( 
         "samples/sample" + str(i) + ".fits", 
-        beam = (0.033, 0.033, 0.0),
+        beam = (0.00166667, 0.00166667, 0.0),
         thresh_isl = 5,
         thresh_pix = 0.5,
         mean_map = "const",
@@ -60,5 +63,15 @@ for i in range( samples.shape[ 0 ] ):
 
     with fits.open( "samples/fit_samples/sample" + str(i) + ".fits" ) as hdul_model:
         model_image_data = hdul_model[ 0 ].data;
-        print( model_image_data );
 
+        fig = plt.figure( figsize=(8, 8) );
+        gs = fig.add_gridspec(1, 1,
+                            left=0.05, right=0.95, bottom=0.1, top=0.95,
+                            wspace=0.5, hspace=0.5);
+        ax = fig.add_subplot( gs[ 0, 0 ] );
+        ax.set_title( 'Model' );
+        img = ax.imshow( model_image_data[ 0, 0, ::-1, : ] );
+        img.set( animated=True );
+        ax.set_xlabel( "dimX" );
+        ax.set_ylabel( "dimY" );
+        plt.show();

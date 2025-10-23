e09ed74137cb3b28eba71e2cde2fda9d0deee605
Ashley Parr <amparr83@gmail.com>
2025-10-23 12:30:35 +0100

    Add class functionality for simple_sample, including arbitrary arguments

--- src/scripts/simple_sample.py
+++ src/scripts/simple_sample.py
@@ -5,18 +5,26 @@ import os
 src_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, src_dir)
 
-
 import model.sampler
 import plotting.image_plots
 
-model_sampler = model.sampler.Sampler();
+class SimpleSample:
+    def __init__(self):
+        # Hook into the in-built sampler
+        self.model_sampler = model.sampler.Sampler()
 
-samples = model_sampler.quick_sample(
-    "LOFAR_model",
-    distribute_model=False,
-    n_samples=1,
-    image_size=80,
-);
+    def run(self, file_name="sample.png", **kwargs):
+        # Generate a sample according to some parameters
+        samples = self.model_sampler.quick_sample(**kwargs)
 
-fig, ax = plotting.image_plots.plot_image_grid( samples[ 0 ] )
-fig.savefig( "sample.png" );
+        # Save to a file
+        fig, ax = plotting.image_plots.plot_image_grid(samples[0])
+        fig.savefig(file_name)
+
+    def quick_run(self):
+        # Generate a sample with default parameters
+        self.run(file_name="sample.png", model_name="LOFAR_model", distribute_model=False, n_samples=1, image_size=80)
+
+# if __name__ == "__main__":
+#     simple_sample = SimpleSample()
+#     simple_sample.quick_run()

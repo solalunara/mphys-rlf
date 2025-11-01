21524dd1730b74d20ee23ca3c71bf14adfedc2a0
Solalunara <greenberg04@gmail.com>
2025-11-01 00:31:50 +0000

    recursive file and log analysis

--- src/scripts/log_analyzer.py
+++ src/scripts/log_analyzer.py
@@ -18,18 +18,18 @@ from scripts.dataset_h5_to_fits import H5ToFitsConverter;
 from scripts.image_analyzer import ImageAnalyzer;
 from scripts.recursive_file_analyzer import RecursiveFileAnalyzer;
 
-class CatalogAnalyzer( RecursiveFileAnalyzer ):
+class LogAnalyzer( RecursiveFileAnalyzer ):
     def FluxCounter( self ):
-        return self.ForEach( CatalogAnalyzer.CalculateHDULFlux, "fits" );
+        return self.ForEach( LogAnalyzer.CalculateFileFlux, "log" );
 
-    def CalculateHDULFlux( hdul: astropy.io.fits.hdu.HDUList ):
-        islands = hdul[ 1 ].data;
-        flux_total = 0;
-        e_flux_total = 0;
-        for island in islands:
-            flux_total += island[ 6 ];
-            e_flux_total += np.sqrt( e_flux_total**2 + island[ 7 ]**2 );
-        return flux_total, e_flux_total;
+    def CalculateFileFlux( filedata: str ):
+        line = "Flux from sum of (non-blank) pixels ..... : ";
+        index = filedata.index( line );
+        index += len( line );
+        endindex = filedata.index( "Jy", index ) - 1;
+        flux = float( filedata[ index:endindex ] );
+        return flux;
+
 
 
 if __name__ == "__main__":
@@ -42,18 +42,18 @@ if __name__ == "__main__":
         dataset_analyzer = ImageAnalyzer( "dataset" );
         dataset_analyzer.AnalyzeAllFITSInInput();
 
-    dataset_catalog_analyzer = CatalogAnalyzer( "pybdsf_catalogs/dataset/" );
-    generated_catalog_analyzer = CatalogAnalyzer( "pybdsf_catalogs/generated/" );
-    dataset_fluxes = np.array( dataset_catalog_analyzer.FluxCounter() ); #both fluxes and flux errors, (N,2)
-    generated_fluxes = np.array( generated_catalog_analyzer.FluxCounter() ); #both fluxes and flux errors, (N,2)
+    dataset_catalog_analyzer = LogAnalyzer( "fits_images/dataset/" );
+    generated_catalog_analyzer = LogAnalyzer( "fits_images/generated/" );
+    dataset_fluxes = np.array( dataset_catalog_analyzer.FluxCounter() );
+    generated_fluxes = np.array( generated_catalog_analyzer.FluxCounter() );
 
     #Use numpy to get a histogram array of the values
     #and matplotlib to plto a log graph from the raw data
     BINCOUNT = 10;
-    d_hist, _ = np.histogram( dataset_fluxes[ :, 0 ], bins=BINCOUNT, range=(0,30) );
-    g_hist, _ = np.histogram( generated_fluxes[ :, 0 ], bins=BINCOUNT, range=(0,30) );
-    d_log_hist, bins, _ = plt.hist( dataset_fluxes[ :, 0 ], density=True, log=True, histtype='step', bins=BINCOUNT, label="dataset", color="b", range=(0,30) );
-    g_log_hist, bins, _ = plt.hist( generated_fluxes[ :, 0 ], density=True, log=True, histtype='step', bins=BINCOUNT, label="generated", color="g", range=(0,30) );
+    d_hist, _ = np.histogram( dataset_fluxes, bins=BINCOUNT, range=(0,30) );
+    g_hist, _ = np.histogram( generated_fluxes, bins=BINCOUNT, range=(0,30) );
+    d_log_hist, bins, _ = plt.hist( dataset_fluxes, density=True, log=True, histtype='step', bins=BINCOUNT, label="dataset", color="b", range=(0,30) );
+    g_log_hist, bins, _ = plt.hist( generated_fluxes, density=True, log=True, histtype='step', bins=BINCOUNT, label="generated", color="g", range=(0,30) );
 
     #Put errorbars on the centre of each bin using the poisson confidence interval
     bin_width = bins[ 1 ] - bins[ 0 ];
---
+++ src/scripts/recursive_file_analyzer.py
@@ -0,0 +1,74 @@
+import os;
+import sys;
+from astropy.io import fits;
+import astropy.io;
+import astropy.stats;
+import astropy.io.fits
+import numpy as np;
+from pathlib import Path;
+import matplotlib.pyplot as plt;
+
+
+# Add the src directory to Python path so we can import modules
+src_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, src_dir)
+
+import scripts.fits_viewer;
+from scripts.dataset_h5_to_fits import H5ToFitsConverter;
+from scripts.image_analyzer import ImageAnalyzer;
+from utils.logging import get_logger;
+
+class RecursiveFileAnalyzer:
+    def __init__( self, path: Path | str ):
+        if path is not Path:
+            path = Path( path );
+        self.path = path;
+        self.counter = 0;
+        self.logger = get_logger( __name__ );
+    
+    def ForEach( self, function, ext: str | None = None, path: Path | str | None = None ):
+        """
+        A method to perform a generic function on all files within a directory given by path, or
+        to read path as a file and perform the generic function on its contents, with optional file extension filtering
+
+        Parameters
+        ----------
+        function : callable
+            The function which will be called on each file in path recursively
+        ext : str | None = None
+            Only call the function and return values of the function on files that match this extension. If none match all files.
+        path : Path | str | None = None
+            The path to the file root, or any subdirectory or file. If none defaults to file root dir.
+
+        Returns
+        -------
+        a list of the return values of the function on each file in path, or the return value acted on a particular file
+        """
+        if path is None:
+            path = self.path;
+            self.counter = 0;
+        if path is not Path:
+            path = Path( path );
+
+        if path.is_dir():
+            return_values = [];
+            for sub_path in path.iterdir():
+                sub_return_values = self.ForEach( function, sub_path );
+
+                #concatenate result lists so we end up with a big 1d array as a result
+                #and toss None values (which we get if the extension doesn't match, or the fn itself returns None)
+                # note - only files can return None, and files can only return one value, so we don't need None checking in array concat
+                if isinstance( sub_return_values, list ):
+                    return_values = return_values + sub_return_values;
+                elif sub_return_values is not None:
+                    return_values.append( sub_return_values );
+            return return_values;
+            
+        else:
+            if ( path.suffix == f".{ext}" ) or ( ext is None ):
+                with open( str( path ), "r" ) as file:
+                    return_value = function( file.read() );
+                self.logger.info( f"image log {self.counter}: {path}" );
+                self.counter += 1;
+                return return_value;
+            else: return;

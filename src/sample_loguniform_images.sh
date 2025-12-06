284b1b50f69bf16a6f08d04120e58379c9696192
Solalunara <greenberg04@gmail.com>
2025-12-06 13:22:09 +0000

    sample loguniform images script

---
+++ src/sample_loguniform_images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=ashley-luna-mphys-rlf
+#SBATCH --constraint=A100
+#SBATCH --time=1-23
+#SBATCH --output=/share/nas2_3/lgreen/logs/out-slurm_%j.out
+#SBATCH --no-requeue
+#SBATCH --array=0-20
+#SBATCH --chdir=/share/nas2_3/lgreen/mphys-rlf
+#SBATCH --cpus-per-task=16
+#SBATCH --exclude=compute-0-9,compute-0-1
+
+set -e
+
+pwd;
+
+echo ">>>activating venv"
+source /share/nas2_3/lgreen/mphys-rlf/.venv/bin/activate
+echo "Array Index:"
+echo $SLURM_ARRAY_TASK_ID
+echo "Array Count:"
+echo $SLURM_ARRAY_TASK_COUNT
+echo ">>>starting program"
+export N_CPUS=$SLURM_CPUS_PER_TASK
+python /share/nas2_3/lgreen/mphys-rlf/src/pybdsf_analysis/generate_fits_files.py --distribution loguniform --upper 1000 --lower 0.0001
+

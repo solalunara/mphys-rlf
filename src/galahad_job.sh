9553cd6574d9edebf0dfd9663e3a933c2c2f30dd
Solalunara <greenberg04@gmail.com>
2025-12-18 11:56:33 +0000

    update galahad scripts with modern versions

--- src/galahad_job.sh
+++ src/galahad_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=ashley-luna-mphys-rlf
+#SBATCH --job-name=mphys-db
 #SBATCH --constraint=A100
 #SBATCH --time=1-23
 #SBATCH --output=/share/nas2_3/lgreen/logs/out-slurm_%j.out
 #SBATCH --no-requeue
-#SBATCH --array=0-20
+#SBATCH --array=0-0
 #SBATCH --chdir=/share/nas2_3/lgreen/mphys-rlf
 #SBATCH --cpus-per-task=16
 #SBATCH --exclude=compute-0-9,compute-0-1
@@ -21,6 +21,6 @@ echo $SLURM_ARRAY_TASK_ID
 echo "Array Count:"
 echo $SLURM_ARRAY_TASK_COUNT
 echo ">>>starting program"
+export HDF5_USE_FILE_LOCKING=FALSE
 export N_CPUS=$SLURM_CPUS_PER_TASK
-python /share/nas2_3/lgreen/mphys-rlf/src/scripts/flux_vs_residual.py
-
+python /share/nas2_3/lgreen/mphys-rlf/hardcastle_catalogue/database_creation.py
--- src/galahad_job_2.sh
+++ src/galahad_job_2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=ashley-luna-mphys-rlf
+#SBATCH --job-name=cplestim
 #SBATCH --constraint=A100
 #SBATCH --time=1-23
 #SBATCH --output=/share/nas2_3/lgreen/logs/out-slurm_%j.out
 #SBATCH --no-requeue
-#SBATCH --array=0-20
+#SBATCH --array=0-0
 #SBATCH --chdir=/share/nas2_3/lgreen/mphys-rlf
 #SBATCH --cpus-per-task=16
-#SBATCH --exclude=compute-0-9,compute-0-1
+#SBATCH --exclude=compute-0-9,compute-0-1,compute-0-2
 
 set -e
 
@@ -23,4 +23,3 @@ echo $SLURM_ARRAY_TASK_COUNT
 echo ">>>starting program"
 export N_CPUS=$SLURM_CPUS_PER_TASK
 python /share/nas2_3/lgreen/mphys-rlf/src/scripts/flux_vs_residual.py
-
